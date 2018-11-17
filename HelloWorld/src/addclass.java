cc94129252720b97f167f9b103fc8ceb31ac31ed
chitti402 <45117737+chitti402@users.noreply.github.com>
2018-11-17 16:37:28 +0530

    if condition added in this class

--- HelloWorld/src/addclass.java
+++ HelloWorld/src/addclass.java
@@ -2,6 +2,10 @@
 public class addclass {
 	public static void main(String args[]) {
 		System.out.println("saved in GIT hub");
+		
+		if (0=0){
+			System.out.println("if condition satisfied");
+			
 	}
 
 }
